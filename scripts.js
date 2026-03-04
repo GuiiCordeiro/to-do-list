fb0b924165be8066a5f0a78896e795d9d8f09b5e
GuiiCordeiro <g.cordeirodossantos@outlook.com>
2026-03-04 13:49:58 -0300

    Salvando com localStorage

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+let tarefas = []
+
+function salvarTarefas(){
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+}
+
 let input = document.querySelector('input')
 
 input.addEventListener("keydown", function(enter){
@@ -6,15 +12,44 @@ input.addEventListener("keydown", function(enter){
     }
 })
 
-function AdicionarTarefa(){
-    const tarefa = input.value
-    const li = document.createElement('li')
-    li.innerHTML = tarefa + "<span onclick='apagarTarefa(this)'>❌</span>"
-    document.querySelector('ul').appendChild(li)
+function AdicionarTarefa() {
+    const tarefa = input.value.trim()
+
+    if (tarefa === "") return
+
+    tarefas.push(tarefa)
+    salvarTarefas()
+    renderizarTarefas()
+
     input.value = ''
     input.focus()
 }
 
-function apagarTarefa(tarefaAtual){
-    tarefaAtual.parentElement.remove()
+function renderizarTarefas() {
+    const ul = document.querySelector('ul')
+    ul.innerHTML = ""
+
+    tarefas.forEach((tarefa, index) => {
+        const li = document.createElement('li')
+        li.innerHTML = `
+            ${tarefa}
+            <span onclick="apagarTarefa(${index})">❌</span>
+        `
+        ul.appendChild(li)
+    })
 }
+
+function apagarTarefa(index) {
+    tarefas.splice(index, 1)
+    salvarTarefas()
+    renderizarTarefas()
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const tarefasSalvas = localStorage.getItem("tarefas")
+
+    if (tarefasSalvas) {
+        tarefas = JSON.parse(tarefasSalvas)
+        renderizarTarefas()
+    }
+})
